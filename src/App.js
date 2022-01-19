88e408420cb04d3bf942f2d5babdec37461481f3
Hakeem <hakeem.trotmanp1@gmail.com>
2022-01-19 16:14:40 -0500

    Finish base skelton of cards, added more stat and card based compontes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 import Value from "./components/Values.js";
+import guide from "./images/mob_images/guide.png";
+import skeletron_day from "./images/mob_images/skeletron_day.jpg";
+import queen_bee from "./images/mob_images/queen-bee.jpg";
 
 //imported mui's
 import Grid from "@mui/material/Grid";
@@ -10,6 +13,7 @@ function App() {
     {
       title: "Queen Bee",
       hp: "3400",
+      image:queen_bee,
       atk: "30",
       def: "8",
       flavor_text: ["Terror Of The Jungle", "Sure she's not a Hornet?", "Ahhhh... The Bees!!"],
@@ -18,6 +22,7 @@ function App() {
     {
       title: "Skeletron(Day)",
       hp: "4400",
+      image:skeletron_day,
       atk: "1000",
       def: "9999",
       flavor_text: ["Guardian Of The Dungeon", "The One Shot Boss", "Dont..."],
@@ -26,6 +31,7 @@ function App() {
     {
       title: "Guide",
       hp: "250",
+      image: guide,
       atk: "8",
       def: "250",
       flavor_text: ["Your First Friend", "Your Guide", "What Does This Doll Do?"],
@@ -40,6 +46,7 @@ function App() {
             <Value
               title={card.title}
               hp={card.hp}
+              image={card.image}
               atk={card.atk}
               def={card.def}
               flavor_text={card.flavor_text}
--- src/components/Values.js
+++ src/components/Values.js
@@ -1,39 +1,48 @@
 import value from "../App";
+import "./card-background.css"
 import "./card_border.css";
 import "./flex-componet.css";
+import "./card-width.css";
+import "./alignment.css"
+import "./npc-image.css"
+
+import "./stats_styles/atk_def.css";
+import "./stats_styles/name.css"
 
 import Avatar from "@mui/material/Avatar";
 import { green, red, blue } from "@mui/material/colors";
 
-function Value({ title, hp, atk, def, flavor_text }) {
+function Value({ title, hp, image, atk, def, flavor_text }) {
   return (
-    <div className="card_border">
-      <h2>{title}</h2>
-      <div className = "flex_componet">
-        <p>HP</p>
-        <Avatar sx={{ bgcolor: green[500] }}>{hp}</Avatar>
+    <div className="background">
+      <div className="card_border">
+        <h2 className="name center">{title}</h2>
+        <div className="flex_componet">
+          <p className="right">HP</p>
+          <Avatar sx={{ bgcolor: green[500] }}>{hp}</Avatar>
+        </div>
 
-      </div>
+        <img className="size" src={image} />
 
-      <div>
-          Picture goes here
-      </div>
+        <div className="right">
+          {flavor_text.map((flavor_text) => {
+            return <li key={flavor_text}> {flavor_text} </li>;
+          })}
+        </div>
 
-
-
-      <div>
-        {flavor_text.map((flavor_text) => {
-          return <li key={flavor_text}> {flavor_text} </li>;
-        })}
-      </div>
-
-      <div className="flex_componet">
+        <div className="flex_componet">
+          <div className="card-width"></div>
+          <div className="card-width"></div>
+          <div className="flex_componet atk_def">
             <Avatar sx={{ bgcolor: red[500] }}>{atk}</Avatar>
             <p>ATK</p>
+          </div>
+          <div className="flex_componet atk_def">
             <Avatar sx={{ bgcolor: blue[500] }}>{def}</Avatar>
             <p>DEF</p>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
